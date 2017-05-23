d49afd367766e08a278ab6e755360c1bbd1a6d96
Warinya640-6 <warinya2539@hotmail.com>
2017-05-23 16:29:53 +0700

    Add files via upload

---
+++ body_detail.php
@@ -0,0 +1,38 @@
+﻿<?php
+ use google\appengine\api\users\User;
+ use google\appengine\api\users\UserService;
+ use google\appengine\api\cloud_storage\CloudStorageTools;
+ 
+ include_once("config.php");  
+ global $appid,$db;  
+ $gid=$_GET['g'];
+ $pid=$_GET['i'];  
+ if(isset($db['items'][$gid][$pid])){
+   $grec=$db['groups'][$gid];
+    $prec=$db['items'][$gid][$pid];   
+  }else{
+   echo "ข้อมูลไม่ถูกต้อง";
+   return;
+ }
+ 
+ echo "<h1>$prec[name] รหัสสินค้า : $pid </h1> ";
+ echo "<h2><div>ราคา : $prec[price] ฿</div></h2>";
+ echo "<div>$prec[detail]</div>";
+ 
+ if(isset($prec['picfile']) && file_exists($prec['picfile'])){
+   $img=CloudStorageTools::getImageServingUrl($prec['picfile'],["size"=>400]);   
+   echo "<img src='$img'>";
+ }
+?>
+
+<?php
+
+if(UserService::isCurrentUserAdmin()){
+ echo "<br><a href='main.php?p=additem&g=$gid&i=$pid'>Edit</a>";
+} 
+
+// debug
+echo "<pre>";
+print_r($prec);
+echo "</pre>";
+?>
